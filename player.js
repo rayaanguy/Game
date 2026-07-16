8ab376a0bbb6f33fa9c698cc6a0f53d1813fc3d1
rayaanguy <rayaanguy@gmail.com>
2026-07-16 14:22:45 +0000

    1hr mark reached currently did js and player.js

--- player.js
+++ player.js
@@ -1,6 +1,12 @@
 class player {
     constructor(game){
         this.game = game ;
+        this.width = 100;
+        this.height = 91.3;
+        this.x = 0;
+        this.y = this.game.height - this.height;
+        this.image = document.getElementById('player');
     }
 }
+
 
--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 window.addEventListener('load', function (){
- const canvas = document.getElementById('canvas');
+ const canvas = document.getElementById('hi');
  const ctx = canvas.getContext('2d');
  canvas.width = 500;
  canvas.height = 500;
+
+ const game = new game(canvas.width, canvas.height);
+ console.log(game);
 });
+
 class game {
     constructor(width, height) {
         this.width = width;
@@ -13,5 +17,8 @@ class game {
 
     }
     draw(){
-
+        if (this.player) {
+            this.player.draw();
+        }
     }
+}
