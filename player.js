1da9988816a71f0605fa8ffe08906d40ce6beb6b
rayaanguy <rayaanguy@gmail.com>
2026-07-16 11:17:06 +0000

    done some basic format now going to add images

---
+++ player.js
@@ -0,0 +1,6 @@
+class player {
+    constructor(game){
+        this.game = game ;
+    }
+}
+
--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 window.addEventListener('load', function (){
  const canvas = document.getElementById('canvas');
  const ctx = canvas.getContext('2d');
- canvas.width = 1500;
- canvas.height = 1500;
+ canvas.width = 500;
+ canvas.height = 500;
 });
+class game {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    update () {
+
+    }
+    draw(){
+
+    }
