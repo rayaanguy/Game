013ea1c022da455c9f6a73a69f41368f1c0f8975
rayaanguy <rayaanguy@gmail.com>
2026-07-23 11:59:40 +0000

    error fixed

--- player.js
+++ player.js
@@ -2,9 +2,9 @@ export class player {
   constructor(game) {
     this.game = game;
     this.width = 100;
-    this.height = 91.3;
-    this.x = 200;
-    this.y = this.game.height - this.height;
+    this.height = 93;
+    this.x = 150;
+    this.y = 200 ;
     this.image = new Image();
     this.image.src = 'player.png';
   }
@@ -12,8 +12,7 @@ export class player {
   update() {}
 
   draw(ctx) {
-      context.fillStyle = 'black';
-      context.fillRect(this.x, this.y, this.width, this.height);
-    }
+    ctx.fillStyle = 'black';
+    ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
--- script.js
+++ script.js
@@ -1,23 +1,13 @@
 import { player } from './player.js';
-
 window.addEventListener('load', function () {
-  const canvas = document.getElementById('hi');
-  const ctx = canvas.getContext('2d');
-  canvas.width = 500;
-  canvas.height = 500;
+ const canvas = document.getElementById('canvas');
+   const ctx = canvas.getContext('2d');
+   canvas.width = 500;
+   canvas.height = 500;
 
-  const game = new Game(canvas.width, canvas.height);
-  console.log(game);
-
-  function animate() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    game.update();
-    game.draw(ctx);
-    requestAnimationFrame(animate);
-  }
-
-  
-});
+    const game = new Game(canvas.width , canvas.height);
+              game.draw(ctx);
+              });
 
 class Game {
   constructor(width, height) {
@@ -26,11 +16,11 @@ class Game {
     this.player = new player(this);
   }
 
-  update() {}
-
-
   draw(context) {
     this.player.draw(context);
   }
-  
+  update(){
+
+  }
 }
+
